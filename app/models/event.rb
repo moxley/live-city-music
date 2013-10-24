214062cb045f3f368893be9044f666da667d6b8f
Moxley Stratton <moxley.stratton@gmail.com>
2013-10-23 22:58:52 -0700

    Validate event title length
    Set city in Mercury parser

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ActiveRecord::Base
   belongs_to :venue
   has_many :artists_events
   has_many :artists, through: :artists_events
+
+  validates :title, length: { maximum: 255 }
 end
--- app/models/mercury_importer.rb
+++ app/models/mercury_importer.rb
@@ -1,4 +1,18 @@
 class MercuryImporter
+  attr_accessor :city
+
+  def initialize(opts={})
+    @city = opts[:city] || 'Portland'
+  end
+
+  def self.for_mercury(opts = {})
+    new opts.merge(city: 'Portland')
+  end
+
+  def self.for_stranger(opts = {})
+    new opts.merge(city: 'Seattle')
+  end
+
   def import(file)
     MercuryParser.new.raw_events_from_file(file).each do |raw_event|
       event = Event.create title: raw_event.title,
@@ -10,19 +24,14 @@ class MercuryImporter
 
   def find_or_create_venue(raw_event)
     loc = raw_event.location
-    event = Venue.where(name: loc.title, city: 'Portland').first_or_initialize
+    event = Venue.where(name: loc.title, city: city).first_or_initialize
     event.attributes = {address_1: loc.address_1, phone: loc.phone}
     event.save!
     event
   end
 
   def find_or_create_artists(raw_event)
-    names = raw_event.title[/^([^:]*:\s*)?(.*)$/, 2] # Remove special event name
-    if names.include?(',')
-      # Replace last 'and' with comma
-      names.gsub!(/ and /, ',')
-    end
-    names = names.split(',').map(&:strip)
+    names = EventParser.artist_names(raw_event.title)
     names.map do |name|
       Artist.find_or_create_by(name: name)
     end
