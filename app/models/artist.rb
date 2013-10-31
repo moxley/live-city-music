e7913617a8927c657e2412947d2e37fe2c33ab37
Moxley Stratton <moxley.stratton@gmail.com>
2013-10-30 21:49:31 -0700

    Artist#played_with

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,12 @@
 class Artist < ActiveRecord::Base
-  acts_as_taggable on: :user_genres, :peer_genres, :venue_genres, :name_genres
+  acts_as_taggable_on :user_genres, :peer_genres, :venue_genres, :name_genres
+
+  has_many :events
+
+  def played_with
+    Artist.
+      joins('JOIN artists_events ae ON ae.artist_id = artists.id AND ae.artist_id != %d' % id).
+      joins('JOIN artists_events ae2 ON ae2.artist_id = %d
+             AND ae2.event_id = ae.event_id' % id)
+  end
 end
