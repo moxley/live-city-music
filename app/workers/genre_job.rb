fcfdada3de61efb9deae700a4c40f5f469eb3ef9
Moxley Stratton <moxley.stratton@gmail.com>
2013-11-09 11:23:34 -1000

    Re-integrate GenreJob with new genre calculation API

--- app/workers/genre_job.rb
+++ app/workers/genre_job.rb
@@ -18,12 +18,12 @@ class GenreJob
 
   def perform_on(ar_class)
     ar_class.all.each do |obj|
-      obj.calculate_genre_points.each do |attrs|
+      obj.calculate_genre.each do |attrs|
         genre = Genre.find_by_name(attrs[:genre_name])
-        gp = GenrePoint.where(target: obj,
-                              genre_id: genre.id,
+        gp = GenrePoint.where(target:     obj,
+                              genre_id:   genre.id,
                               point_type: attrs[:point_type],
-                              source: attrs[:source]).
+                              source:     attrs[:source]).
                         first_or_initialize
         gp.value = attrs[:value]
         gp.save!
