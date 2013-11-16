ab935edcfac78fd5aa667453665143c90e48fe23
Moxley Stratton <moxley.stratton@gmail.com>
2013-11-16 10:29:16 -1000

    Spread out genre calculation job over an hour

--- app/workers/genre_job.rb
+++ app/workers/genre_job.rb
@@ -17,6 +17,18 @@ class GenreJob
   end
 
   def perform_on(ar_class)
-    ar_class.all.each(&:calculate_and_apply_genres)
+    ar_class.pluck(:id).each do |id|
+      seconds = rand(3600)
+      InstanceGenreJob.perform_in(seconds, ar_class.to_s, id)
+    end
+  end
+
+  class InstanceGenreJob
+    include Sidekiq::Worker
+
+    def perform(ar_class_s, id)
+      instance = Object.const_get(ar_class_s).find(id)
+      instance.calculate_and_apply_genres
+    end
   end
 end
