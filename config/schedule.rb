d09b45064ff91359f82603fa703073b6d6218983
Moxley Stratton <moxley.stratton@gmail.com>
2013-11-24 16:56:34 -0800

    Add jobs to schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,19 +1,14 @@
 # http://en.wikipedia.org/wiki/Cron
+# Learn more: http://github.com/javan/whenever
 
 set :output, "/var/www/bandlist/shared/log/cron_log.log"
 
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+# 4:00pm PDT
+every :day, at: '00:00' do
+  rake 'collect_pages'
+end
 
-# Learn more: http://github.com/javan/whenever
-
-every 1.minute do
-  command "echo 'heartbeat' > /var/www/bandlist/shared/log/production.log"
+# 4:30pm PDT
+every :day, at: '00:30' do
+  rake 'calculate_genres'
 end
