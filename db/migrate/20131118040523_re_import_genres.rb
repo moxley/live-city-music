7e1d2913f43749bc469845a96226e479936d9b80
Moxley Stratton <moxley@theclymb.com>
2013-11-29 23:47:04 -0800

    properly sort list of styles

--- db/migrate/20131118040523_re_import_genres.rb
+++ db/migrate/20131118040523_re_import_genres.rb
@@ -3,7 +3,7 @@ class ReImportGenres < ActiveRecord::Migration
     return if Rails.env == 'test'
     execute "TRUNCATE genre_points RESTART IDENTITY"
     execute "TRUNCATE genres RESTART IDENTITY"
-    Dir.glob('db/list_of_styles*.html').each do |path|
+    Dir.glob('db/list_of_styles*.html').sort.each do |path|
       Genre.import_from_file(File.new(path))
     end
   end
