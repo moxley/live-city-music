ae44af678e011aba2e20ab5c0c04cfb4d82e524d
Moxley Stratton <moxley@theclymb.com>
2013-12-23 23:09:44 -0800

    Fix schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,16 +4,16 @@
 set :output, "/var/www/bandlist/shared/log/cron_log.log"
 
 # 00:05 & 12:05 Pacific
-every :day, at: '05 08,19 * * *' do
+every '05 08,20 * * *' do
   rake 'pages:download'
 end
 
 # 00:30 & 12:30 Pacific
-every :day, at: '30 08,20 * * *' do
+every '30 08,20 * * *' do
   rake 'updates:artists_sources'
 end
 
 # 00:45 & 12:45 Pacific
-every :day, at: '45 08,20 * * *' do
+every '45 08,20 * * *' do
   rake 'calculate:genres'
 end
